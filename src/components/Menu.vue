49aeef4c6bd4f93dfe25ea69f52f3400b01571c4
FabricioHrdz <95989854+FabricioHrdz@users.noreply.github.com>
2025-09-16 18:07:07 -0600

    Menu.vue

---
+++ src/components/Menu.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <!-- Menú fijo -->
+    <Menu />
+
+    <!-- Contenido principal -->
+    <div class="content">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <h1>Bienvenido a Vue.js</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
+}
+</script>
+
+<style>
+/* Estilos del app */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.content {
+  margin-top: 80px;
+}
+
+</style>
